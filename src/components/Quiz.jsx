c43881a4135c7b84f5593ac2a28f74f245114ad9
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-02-09 14:55:37 +0300

    feat: display questions and completion board

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
+
+function Quiz(props) {
+
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevState) => [...prevState, selectedAnswer]);
+    }
+    if (quizIsComplete) {
+        return (
+            <div id='summary'>
+                <img src={quizCompleteImg} alt="quiz-completed"/>
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    return (
+        <div id='quiz'>
+            <div id='question'>
+                <p>{QUESTIONS[activeQuestionIndex].text}</p>
+                <ul id='answers'>
+                    {shuffledAnswers.map((answer) => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Quiz;
