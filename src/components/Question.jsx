57f252f6bffc864aeab3812ea2fa46b7535a8a39
Anton Ziuzikov <zuzikov.anton@gmail.com>
2024-02-12 14:23:12 +0300

    feat: split quiz component into two sibblings to better re-rendering

---
+++ src/components/Question.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import QuestionTimer from "./QuestionTimer.jsx";
+import Answers from "./Answers.jsx";
+
+function Question({questionText,
+                      answers,
+                      onSelectAnswer,
+                      selectedAnswer,
+                      answerState,
+                      onSkipAnswer})
+{
+    return (
+        <div id='question'>
+            <QuestionTimer timeout={10000} onTimeout={onSkipAnswer}/>
+            <h2>{questionText}</h2>
+            <Answers answers={answers}
+                     selectedAnswer={selectedAnswer}
+                     answerState={answerState}
+                     onSelect={onSelectAnswer}/>
+        </div>
+    );
+}
+
+export default Question;
